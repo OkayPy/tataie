b5265530832ba4719910707887e96ade0e454477
OkayPy <david.discord.official@gmail.com>
2021-07-06 13:58:54 +0300

    Create trashtalk.js

---
+++ src/commands/fun/trashtalk.js
@@ -0,0 +1,21 @@
+const usage = require("../../../utils/usage.js"); //betters help-messages
+const { prefix } = require("../../loaders/reader") //get prefix from botconfig
+
+module.exports = {
+    config: {
+        name: "meme",
+        aliases: ["memes", "mem"],
+        usage: "$meme",
+        description: "niste memeuri de cacat",
+        permissions: "none"
+    },
+    run: async (bot, message, args) => {
+    
+        for (let i = 0; i < cars.length; i++) {
+            message.channel.send("fututi rasa matii cosmic de bot prost easy sotron ");
+            message.author.send("de ce esti autist");
+          }
+
+
+    }
+}
