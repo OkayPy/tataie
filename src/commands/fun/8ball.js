94a2bbec01261f3e98bcc273a08ef911bc1c406f
OkayPy <david.discord.official@gmail.com>
2021-07-07 20:21:54 +0300

    Update 8ball.js

--- src/commands/fun/8ball.js
+++ src/commands/fun/8ball.js
@@ -16,7 +16,7 @@ module.exports = {
         if(!args[2]) return message.channel.send(`pune o intrebare. \n sintaxa comezii: ${cmd} (intrebare)`);
         //questions should be at least 3 words long
 
-        let replies = ["da.", "nu.", "nush coaie.", "mai taziu ca nam chef acuma!", "refuz", "nu, tactu ala ciung haha", "asta a zis si ma-ta", `${message.author.username}, esti un bot`];
+        let replies = ["da.", "nu.", "nush coaie.", "mai taziu ca nam chef acuma!", "refuz", "nu, tactu ala ciung haha", "asta a zis si ma-ta", "asa se zice pe la mine prin sat", `${message.author.username}, esti un bot`, "sunt ocupat sa dau in mata", "nu prea"];
         let result = Math.floor((Math.random() * replies.length)); 
         //get random number between 0 and the length of the array
 
